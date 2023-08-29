b7755d9d8cd1dc0d1b5fabb8c78caf6d708234a3
bhupey <bhupendra.neupane08@gmail.com>
2023-08-29 16:21:33 +0545

    function order

---
+++ Day14/task2.py
@@ -0,0 +1,12 @@
+"""
+Create a new list of repeated items from a provided list:
+nums = [3, 4, 2, 2, 1, 3, 3, 3]
+Output = [3, 2]
+"""
+
+nums = [3, 4, 2, 2, 1, 3, 3, 3]
+rep = []
+for num in nums:
+    if nums.count(num) > 1 and num not in rep:
+        rep.append(num)
+print(rep)
